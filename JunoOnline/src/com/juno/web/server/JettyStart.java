f90e1c6a3e00eead817394b4f0f7ed309cbbe3a2
Larry <larrycai1984@outlook.com>
2015-09-14 11:02:19 +0800

    Include Maven Jetty Spring MVC framework

---
+++ JunoOnline/src/com/juno/web/server/JettyStart.java
@@ -0,0 +1,34 @@
+package com.juno.web.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+
+public class JettyStart extends AbstractHandler{
+
+    @Override
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+ 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new JettyStart());
+  
+        server.start();
+        server.join();
+    }
+
+}
